f1a2de04824c4e52cc718d10ab39ea639daa6fb0
henryyusuf <baharhenry10@gmail.com>
2025-05-31 15:55:43 +0700

    versioning api

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
+use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,8 +9,7 @@ use Illuminate\Support\Facades\Route;
 // tickets
 // users
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
