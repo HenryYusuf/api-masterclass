575735acf3e410ee6c88a5e4f6dcdf7b56ed99d0
henryyusuf <baharhenry10@gmail.com>
2025-05-31 15:38:08 +0700

    designing the url

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,21 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// {domain}/api/tickets/{id}/{action(store/update/delete)}
+// universal resource locator
+// tickets
+// users
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
