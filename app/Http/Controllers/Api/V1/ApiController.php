772bfffabf232c63bd0e297e2baf5de03189531c
henryyusuf <baharhenry10@gmail.com>
2025-06-07 14:05:02 +0700

    changes: using policies for user authorization

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ApiController extends Controller
 {
     use ApiResponses;
+
+    protected $policyClass;
 
     public function include(string $relationship)
     {
@@ -21,5 +24,9 @@ class ApiController extends Controller
         $includeValue = explode('.', strtolower($param));
 
         return in_array(strtolower($relationship), $includeValue);
+    }
+
+    public function isAble($ability, $targetModel) {
+        return Gate::authorize($ability, [$targetModel, $this->policyClass]);
     }
 }
